bd852df08c8f593067aa1ab6e1df763442f49c0f
Vaibhav <tyagivaibhav520@gmail.com>
2025-07-07 11:55:54 +0530

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
 'use client'
 import {  useEffect } from "react";
 import HeroSection from "@/components/Home/HeroSection";
-import { useAppDispatch} from "@/hooks/ReduxHooks";
+import { useAppDispatch, useAppSelector} from "@/hooks/ReduxHooks";
 import Layout from "@/Layout/Layout";
 import { fetchCarBlogPosts } from "@/redux/slices/carSlice";
 import { BlogLayout } from "@/components/Home/BlogLayout";
 import { NewTechnologySection } from "@/components/Home/NewTechnologySection";
 import { CategorySection } from "@/components/Home/CategorySection";
+import Loader from "./loading";
+import ErrorPage from "./error";
 
 
 export default function Home() {
+  const data=useAppSelector(state=>state.carBlogPosts)
   const dispatch=useAppDispatch()
     useEffect(() => {
         dispatch(fetchCarBlogPosts());
       }, [dispatch]);
        
+   if (data.status === 'loading') return <Loader scrolling={false} />;
+   if (data.error) return <ErrorPage />;
  
   return (
     <Layout>
